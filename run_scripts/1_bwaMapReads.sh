7a7570ea8a72af8a3b85a56f28df501d1a1d0956
fredjaya <fredjaya1@gmail.com>
2020-07-31 10:19:48 +1000

    running bwa mem on all samples. 8/16 completed - resources okgit add --all

--- run_scripts/1_bwaMapReads.sh
+++ run_scripts/1_bwaMapReads.sh
@@ -2,7 +2,7 @@
 
 #PBS -P RDS-FSC-Scape-RW
 #PBS -l select=1:ncpus=1:mem=1GB
-#PBS -l walltime=120:00:00
+#PBS -l walltime=72:00:00
 
 WORKDIR=/scratch/Scape/fred/2008_manual
 NFDIR=/home/fjay0039/mutscape
@@ -12,5 +12,4 @@ module load nextflow/20.04.1
 
 nextflow run ${NFDIR}/main.nf \
 	--mode bwaMapReads \
-	-profile pbs \
-	--ref "/project/Scape/Trimmomatic/Paired/Fdrone_{R1,R2}_paired.fastq.gz"
+	-profile pbs
